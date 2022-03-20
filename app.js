6869f516f18b1553663ef0d899ca083dcce37155
Tatum-Leigh <tatumdevries@yahoo.com>
2022-03-20 22:47:17 +0800

    Non-functional virtual keyboard added. Board added as well, the board accepts input from the keyboard, but has no backspace or enter functionality right now.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+
 // Generate a word from five letter wordbank 
 
 function getRandomIntInclusive(min, max) {
@@ -10,3 +15,64 @@ function getRandomIntInclusive(min, max) {
 const LENGTH = words.length 
 var random_index = getRandomIntInclusive(0,LENGTH); 
 console.log(words[random_index]);
+
+// Board // 
+function initBoard() {
+  let board = document.getElementById("game-board");
+
+  for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+      let row = document.createElement("div")
+      row.className = "tile-row"
+      
+      for (let j = 0; j < 5; j++) {
+          let box = document.createElement("div")
+          box.className = "letter-tile"
+          row.appendChild(box)
+      }
+
+      board.appendChild(row)
+  }
+}
+
+initBoard()
+
+// Verifies if the key is an acceptable alphanumerical character //
+document.addEventListener("keyup", (e) => {
+
+  if (guessesRemaining === 0) {
+      return
+  }
+
+  let pressedKey = String(e.key)
+  if (pressedKey === "Backspace" && nextLetter !== 0) {
+      deleteLetter()
+      return
+  }
+
+  if (pressedKey === "Enter") {
+      checkGuess()
+      return
+  }
+
+  let found = pressedKey.match(/[a-z]/gi)
+  if (!found || found.length > 1) {
+      return
+  } else {
+      insertLetter(pressedKey)
+  }
+})
+
+// Inserting letters //
+function insertLetter (pressedKey) {
+  if (nextLetter === 5) {
+      return
+  }
+  pressedKey = pressedKey.toLowerCase()
+
+  let row = document.getElementsByClassName("tile-row")[6 - guessesRemaining]
+  let box = row.children[nextLetter]
+  box.textContent = pressedKey
+  box.classList.add("filled-box")
+  currentGuess.push(pressedKey)
+  nextLetter += 1
+}
